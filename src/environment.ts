ec7bc8d6a72433e37468b1c4b42784a561aab4e8
Marcos Javier Alvarez Maestre <garusis@gmail.com>
2024-08-29 05:36:22 -0500

    fix: Make envs more flexible

--- src/environment.ts
+++ src/environment.ts
@@ -29,9 +29,7 @@ const envSchema = z.object({
   GC_TASK_CHAT_QUEUE: trimmedStringSchema,
   GC_SERVICE_ACCOUNT_EMAIL: trimmedStringSchema,
   OPENAI_CHAT_MODEL: z.string(),
-  TIKTOKEN_CHAT_MODEL: z
-    .enum(["gpt-3.5-turbo", "gpt-4"])
-    .default("gpt-3.5-turbo"),
+  TIKTOKEN_CHAT_MODEL: z.string(),
   OPENAI_TRANSCRIPTION_MODEL: z.literal("whisper-1"),
   OPENAI_MAX_TOKENS: stringNumberSchema,
   OPENAI_MAX_RESPONSE_TOKENS: stringNumberSchema,
